1aea9bc0aa491fd9abec6dbc23c1fa8acf6ff485
Oleg Novitskiy <surkiss2@gmail.com>
2021-03-15 19:25:23 +0200

    change api key

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const Parser = require('./Parser');
 const parser = new Parser();
 parser.init();
 
 const { Telegraf, Extra } = require('telegraf');
-// const { GOOGLE_CLOUD_PROJECT_ID, BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
+const { BOT_TOKEN } = process.env;
 
-const bot = new Telegraf('1654602084:AAFeNIKC2MeBf9jRz30gQPVx4LUpOCAZW2Y');
-// const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf(BOT_TOKEN);
 
 bot.on('text', (ctx) => {
     const { text } = ctx.message;
@@ -22,11 +21,4 @@ bot.on('text', (ctx) => {
 
 bot.launch();
 
-// bot.telegram.setWebhook(
-//     `https://${GOOGLE_CLOUD_REGION}-${GOOGLE_CLOUD_PROJECT_ID}.cloudfunctions.net/${process.env.FUNCTION_TARGET}` //FUNCTION_TARGET is reserved Google Cloud Env
-// );
-
-// exports.telegramBotWebhook = (req, res) => {
-//     bot.handleUpdate(req.body, res);
-// };
 

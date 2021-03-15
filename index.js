07810df7f71130ce2c2e0e888778e1656b87b818
Oleg Novitskiy <surkiss2@gmail.com>
2021-03-15 09:04:35 +0200

    now using telegraf instead of node-telegram-api

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-const TelegramBot = require('node-telegram-bot-api');
-// const token = '1654602084:AAFeNIKC2MeBf9jRz30gQPVx4LUpOCAZW2Y';
-const { GOOGLE_CLOUD_PROJECT_ID, TELEGRAM_BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
-
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
-
+require('dotenv').config();
 
 const Parser = require('./Parser');
 const parser = new Parser();
-parser.init().then(run);
+parser.init();
 
+const { Telegraf, Extra } = require('telegraf');
+const { GOOGLE_CLOUD_PROJECT_ID, TELEGRAM_BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
 
-bot.telegram.setWebhook(
-    `https://${GOOGLE_CLOUD_REGION}-${GOOGLE_CLOUD_PROJECT_ID}.cloudfunctions.net/${process.env.FUNCTION_TARGET}`
-);
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 
-function run() {
-    bot.on('message', sendParsedData);
+bot.on('text', (ctx) => {
+    const { text } = ctx.message;
+    parser.parseUrls(text).then((res, rej) => {
+        ctx.replyWithHTML(res, Extra.webPreview(false));
+    })
+        .catch(err => {
+            ctx.reply("ERROR!");
+        });
+});
 
-    async function sendParsedData(msg) {
-        const { id: chatID } = msg.chat;
-        const { text } = msg;
-        const formattedText = await parser.parseUrls(text);
-        const config = {
-            parse_mode: 'HTML',
-            disable_web_page_preview: true
-        }
-        bot.sendMessage(chatID, formattedText, config);
-    }
-}
+bot.launch();
 
-exports.telegramBotWebhook = (req, res) => {
-    bot.handleUpdate(req.body, res);
-};
+// bot.telegram.setWebhook(
+//     `https://${GOOGLE_CLOUD_REGION}-${GOOGLE_CLOUD_PROJECT_ID}.cloudfunctions.net/${process.env.FUNCTION_TARGET}` //FUNCTION_TARGET is reserved Google Cloud Env
+// );
 
+// exports.telegramBotWebhook = (req, res) => {
+//     bot.handleUpdate(req.body, res);
+// };
 

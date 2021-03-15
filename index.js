ca14b3c88e5632e49cf97f6a3707a64c5bb0c08f
Oleg Novitskiy <surkiss2@gmail.com>
2021-03-15 10:25:45 +0200

    rollback to telegraf version

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const Parser = require('./Parser');
-// const parser = new Parser();
-// parser.init();
+const Parser = require('./Parser');
+const parser = new Parser();
+parser.init();
 
-// const { Telegraf, Extra } = require('telegraf');
-const { Composer } = require('micro-bot');
-// const { GOOGLE_CLOUD_PROJECT_ID, TELEGRAM_BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
+const { Telegraf, Extra } = require('telegraf');
+const { GOOGLE_CLOUD_PROJECT_ID, BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
 
-const bot = new Composer();
+const bot = new Telegraf(BOT_TOKEN);
 
-bot.start((ctx) => {
-    ctx.reply("bot started");
-})
+bot.on('text', (ctx) => {
+    const { text } = ctx.message;
+    parser.parseUrls(text).then((res, rej) => {
+        ctx.replyWithHTML(res, Extra.webPreview(false));
+    })
+        .catch(err => {
+            ctx.reply("ERROR!");
+        });
+});
 
-// bot.on('text', (ctx) => {
-//     const { text } = ctx.message;
-//     ctx.reply("test");
-//     // parser.parseUrls(text).then((res, rej) => {
-//     //     ctx.replyWithHTML(res, Extra.webPreview(false));
-//     // })
-//     //     .catch(err => {
-//     //         ctx.reply("ERROR!");
-//     //     });
-// });
-
-module.exports = bot;
-
-// bot.launch();
+bot.launch();
 
 // bot.telegram.setWebhook(
 //     `https://${GOOGLE_CLOUD_REGION}-${GOOGLE_CLOUD_PROJECT_ID}.cloudfunctions.net/${process.env.FUNCTION_TARGET}` //FUNCTION_TARGET is reserved Google Cloud Env
@@ -36,7 +28,4 @@ module.exports = bot;
 // exports.telegramBotWebhook = (req, res) => {
 //     bot.handleUpdate(req.body, res);
 // };
-
-// stark-bastion-63584
-// https://stark-bastion-63584.herokuapp.com/
 

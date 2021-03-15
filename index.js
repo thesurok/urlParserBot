adef3619392bdaaf44d211ca8dd7bf6498117568
Oleg Novitskiy <surkiss2@gmail.com>
2021-03-15 22:55:35 +0200

    replace token

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const Parser = require('./Parser');
 const parser = new Parser();
 
 const { Telegraf } = require('telegraf');
-const { BOT_TOKEN, BOT_DOMAIN, THROWAWAY_TOKEN } = process.env;
+const { BOT_TOKEN, BOT_DOMAIN } = process.env;
 
-console.log(THROWAWAY_TOKEN);
-console.log(THROWAWAY_TOKEN === '1639388823:AAEJfhDqKeiYyznBbyDArdGo2c8SH6PPBj4');
-
-const bot = new Telegraf(THROWAWAY_TOKEN);
+const bot = new Telegraf(BOT_TOKEN);
 
 parser.init();
 

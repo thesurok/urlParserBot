dfd3ef3876452f0784f1485c77bd2e0ca974755e
Oleg Novitskiy <surkiss2@gmail.com>
2021-03-15 20:14:37 +0200

    ts3

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const parser = new Parser();
 parser.init();
 
 const { Telegraf, Extra } = require('telegraf');
-const { BOT_TOKEN } = process.env;
+// const { BOT_TOKEN } = process.env;
 
-const bot = new Telegraf(BOT_TOKEN);
-console.log(BOT_TOKEN);
-bot.on('text', (ctx) => {
-    const { text } = ctx.message;
-    parser.parseUrls(text).then((res, rej) => {
-        ctx.replyWithHTML(res, Extra.webPreview(false));
-    })
-        .catch(err => {
-            ctx.reply("ERROR!");
-        });
-});
+// const bot = new Telegraf(BOT_TOKEN);
+// console.log(BOT_TOKEN);
+// bot.on('text', (ctx) => {
+//     const { text } = ctx.message;
+//     parser.parseUrls(text).then((res, rej) => {
+//         ctx.replyWithHTML(res, Extra.webPreview(false));
+//     })
+//         .catch(err => {
+//             ctx.reply("ERROR!");
+//         });
+// });
 
-bot.launch();
+// bot.launch();
 
 

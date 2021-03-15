fed0a821e4fa55787c90dd1425dd5e457b3992d4
Oleg Novitskiy <surkiss2@gmail.com>
2021-03-15 12:26:58 +0200

    move key out of env

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const Parser = require('./Parser');
-const parser = new Parser();
-parser.init();
+// const Parser = require('./Parser');
+// const parser = new Parser();
+// parser.init();
 
 const { Telegraf, Extra } = require('telegraf');
-const { GOOGLE_CLOUD_PROJECT_ID, BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
+// const { GOOGLE_CLOUD_PROJECT_ID, BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
 
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf('1654602084:AAFeNIKC2MeBf9jRz30gQPVx4LUpOCAZW2Y');
+// const bot = new Telegraf(BOT_TOKEN);
 
 bot.on('text', (ctx) => {
+    ctx.reply("test123");
     const { text } = ctx.message;
-    parser.parseUrls(text).then((res, rej) => {
-        ctx.replyWithHTML(res, Extra.webPreview(false));
-    })
-        .catch(err => {
-            ctx.reply("ERROR!");
-        });
+    // parser.parseUrls(text).then((res, rej) => {
+    //     ctx.replyWithHTML(res, Extra.webPreview(false));
+    // })
+    //     .catch(err => {
+    //         ctx.reply("ERROR!");
+    //     });
 });
 
 bot.launch();

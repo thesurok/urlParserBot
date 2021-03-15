2275ef6049f96f0f3f8a640e0998d9c8227ce368
Oleg Novitskiy <surkiss2@gmail.com>
2021-03-15 12:30:33 +0200

    enable parser

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // require('dotenv').config();
 
-// const Parser = require('./Parser');
-// const parser = new Parser();
-// parser.init();
+const Parser = require('./Parser');
+const parser = new Parser();
+parser.init();
 
 const { Telegraf, Extra } = require('telegraf');
 // const { GOOGLE_CLOUD_PROJECT_ID, BOT_TOKEN, GOOGLE_CLOUD_REGION } = process.env;
@@ -11,14 +11,13 @@ const bot = new Telegraf('1654602084:AAFeNIKC2MeBf9jRz30gQPVx4LUpOCAZW2Y');
 // const bot = new Telegraf(BOT_TOKEN);
 
 bot.on('text', (ctx) => {
-    ctx.reply("test123");
     const { text } = ctx.message;
-    // parser.parseUrls(text).then((res, rej) => {
-    //     ctx.replyWithHTML(res, Extra.webPreview(false));
-    // })
-    //     .catch(err => {
-    //         ctx.reply("ERROR!");
-    //     });
+    parser.parseUrls(text).then((res, rej) => {
+        ctx.replyWithHTML(res, Extra.webPreview(false));
+    })
+        .catch(err => {
+            ctx.reply("ERROR!");
+        });
 });
 
 bot.launch();
